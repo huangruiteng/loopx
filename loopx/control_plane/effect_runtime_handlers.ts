037f7f2ff9650ea3b8fd3fc1d9050256f1a6f0d4
huangruiteng <huangrt01@163.com>
2026-09-11 21:38:09 +0800

    feat(context): provide capability guidance at coordinator lifecycle boundaries
    Signed-off-by: huangruiteng <huangrt01@163.com>

--- loopx/control_plane/effect_runtime_handlers.ts
+++ loopx/control_plane/effect_runtime_handlers.ts
@@ -1,3 +1,4 @@
+import { evaluateSubagentContext, describeSubagentContext } from "./subagent_context.ts";
 import {
   effectIdsMatch,
   effectProgramFromOrderedSteps,
@@ -534,6 +535,14 @@ export function createEffectRuntimeHandlers(
     [
       "governed_capability.settlement_status",
       (params) => governedCapabilitySettlementStatus(params.failure),
+    ],
+    [
+      "capability_hook.agent_context.describe",
+      () => describeSubagentContext(),
+    ],
+    [
+      "capability_hook.agent_context.project",
+      (params) => evaluateSubagentContext(params),
     ],
     [
       "capability_hook.interaction_projection.validate_registration",
